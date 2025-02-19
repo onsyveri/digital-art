3f842a79b1c3209f808a845779081a6e5f162bdb
onsyveri <ole.syverinsen@googlemail.com>
2025-02-19 15:20:16 +0100

    Still trying to fix github pages

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig = {
   output: "export", // Gjør Next.js statisk
   basePath: isProd ? "/digital-art" : "", // Erstatt med repo-navnet ditt
   assetPrefix: isProd ? "/digital-art/" : "",
-  images: { unoptimized: true } // Deaktiver Next.js Image Optimization
+  images: { unoptimized: true }, // Deaktiver Next.js Image Optimization
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
