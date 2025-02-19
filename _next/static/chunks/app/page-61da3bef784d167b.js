015b77a3151d2150136f5cc677ef03a12f7bebc6
onsyveri <ole.syverinsen@googlemail.com>
2025-02-19 16:51:58 +0100

    Updates

---
+++ _next/static/chunks/app/page-61da3bef784d167b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2064:(e,_,p)=>{Promise.resolve().then(p.t.bind(p,7574,23))},7574:e=>{e.exports={page:"page_page__ZU32B"}}},e=>{var _=_=>e(e.s=_);e.O(0,[923,465,743,358],()=>_(2064)),_N_E=e.O()}]);
